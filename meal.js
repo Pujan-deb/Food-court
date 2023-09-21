3b44933f15ca29aba47aa76dc815f7621d933f66
Pujan Deb Nath <120366627+Pujan-deb@users.noreply.github.com>
2023-09-22 01:07:06 +0600

    Update meal.js

--- meal.js
+++ meal.js
@@ -6,7 +6,7 @@ const load = (searchtect) => {
         .then(data => show(data.meals))
 }
 show = (data) => {
-    console.log(data.length)
+    console.log(data)
     const sec = document.getElementById('maindiv');
     sec.innerHTML = '';
     const showall = document.getElementById('ShowAll');
@@ -21,7 +21,7 @@ show = (data) => {
         div.innerHTML = `<div class="bg-green-100 rounded-3xl p-2 shadow-xl" id="Foods">
         <img src="${item.strMealThumb}" alt="" class="rounded-3xl mx-auto">
         <h3 class="my-4 text-center text-3xl font-semibold">${item.strMeal}</h3>
-        <p class="my-4 text-center text-xl font-normal">A taste you'll remember.</p>
+        <p class="my-4 text-center text-xl font-normal">${item.strCategory},${item.strMeasure1}</p>
         <button class="text-xl font-medium py-2 px-4 bg-sky-300 rounded-lg w-full" onclick="Showmodal(${item.idMeal})">More</button>
         </div>`
             ;
@@ -43,7 +43,6 @@ document.getElementById('search-box').addEventListener('keypress', () => {
 // })
 
 const Showmodal = (data) => {
-    document.getElementById('modalSection').classList.remove("hidden");
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${data}`;
     fetch(url)
         .then(res => res.json())
@@ -51,8 +50,9 @@ const Showmodal = (data) => {
 
 }
 const displayModal = (info1, info2) => {
-    document.getElementById('foodDesc').innerHTML = info1;
+    document.getElementById('foodDesc').innerHTML = info1.slice(0, 500);
     document.getElementById('modal-title').innerHTML = info2;
+    document.getElementById('modalSection').classList.remove("hidden");
 }
 
 document.getElementById('ShowAll').addEventListener('click', () => {
@@ -77,7 +77,7 @@ showAllData = (data) => {
         div.innerHTML = `<div class="bg-green-100 rounded-3xl p-2 " id="Foods">
         <img src="${item.strMealThumb}" alt="" class="rounded-3xl mx-auto">
         <h3 class="my-4 text-center text-3xl font-semibold">${item.strMeal}</h3>
-        <p class="my-4 text-center text-xl font-normal">A taste you'll remember.</p>
+        <p class="my-4 text-center text-xl font-normal">${item.strCategory},${item.strMeasure1}</p>
         <button class="text-xl font-medium py-2 px-4 bg-sky-300 rounded-lg w-full" onclick="Showmodal(${item.idMeal})">More</button>
         </div>`
             ;
